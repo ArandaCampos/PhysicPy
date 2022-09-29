ccb67face1000a8ca06842bfdbbfb7cc59106d6f
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-09-29 14:12:04 -0300

    upgrade projetil.py

--- phisic_py/cinetica/projetil.py
+++ phisic_py/cinetica/projetil.py
@@ -1,86 +1,120 @@
-from wsgiref.simple_server import WSGIRequestHandler
+#---------------------------------------------
+#   Author: Renan Campos
+#   Github: github.com/ArandaCampos
+#   Projéteis
+#---------------------------------------------
+
 import pygame
 import math
-pygame.init()
-
-altura = float(input('Qual a altura o objeto a ser lançado? (metros) '))
-velocidade = float(input('Qual a velocidade inicial (m/s): '))
-angulo = float(input('Qual o angulo que o objeto será lançado? (graus) '))
+from base import Window
 
 WIDTH, HEIGHT = 1200, 600
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Projétil")
-
-WHITE = (210, 210, 210)
-BLACK = (0, 0, 0, .8)
-
-FONT = pygame.font.SysFont("comicsans", 16)
+WHITE, BLACK, GREY = (210, 210, 210), (0, 0, 0, .8), (128, 128, 128)
 
 class Objeto:
 	g = -9.807 		       	# Aceleração da gravidade (m/s^2)
 	ESCALA = 10				# 10px == 1 metro
 
-	def __init__(self, x, y, velocidade, angulo):
-		self.x = x													# Posição Vertical (m)
-		self.y = y													# Posição Horizontal (m)
-		self.vy = velocidade * math.sin(math.radians(angulo))		# Velocidade Inicial eixo y
-		self.vx = velocidade * math.cos(math.radians(angulo))		# Velocidade Inicial eixo x
-		self.angulo = angulo
-		self.diametro = 20 			# Raio (m)
-		self.orbita = []
+	def __init__(self, h, velocity, angle):
+		self.x = 0											# Posição Vertical (m)
+		self.y = h													# Posição Horizontal (m)
+		self.vy = velocity * math.sin(math.radians(angle))		# Velocidade Inicial eixo y
+		self.vx = velocity * math.cos(math.radians(angle))		# Velocidade Inicial eixo x
+		self.diameter = 20 			# Raio (m)
+		self.movements = []
+		self.position = 0
+		self.SCALE = 0
 
 	def draw(self, win):
-		updated_points = []
-		for point in self.orbita:
-			x, y = point
-			x = x
-			y = y
-			updated_points.append((x, y))
+		if self.position > 2:
+			points = []
+			for movement in self.movements[0: self.position]:
+				points.append(self.transform(movement))
+			pygame.draw.lines(win, GREY, False, points, 2)
 
-		if len(self.orbita) > 2:
-			pygame.draw.lines(win, BLACK, False, updated_points, 2)
+		pygame.draw.circle(win, BLACK, self.transform(self.movements[self.position]), self.diameter)
 
-		pygame.draw.circle(win, BLACK, (x, y), self.diametro)
+	def transform(self, position):
+		self.SCALE = WIDTH / self.movements[-1][0]
+		x, y = position
+		return (x * self.SCALE - self.diameter, HEIGHT - y * self.SCALE - self.diameter)
 
-	def velocidade(self, t):
-		vx, vy, g = self.vx, self.vy, self.g
-		Vx = vx * t
-		Vy = vy * t + g * math.pow(t, 2) / 2
+	def velocity(self, t):
+		Vx = self.vx * t
+		Vy = self.vy * t + self.g * math.pow(t, 2) / 2
 		return Vx, Vy
 
-	def deslocamento(self, t):
-		vx, vy = self.velocidade(t)
-		x, y = self.x, self.y
-		px = x + vx
-		py = y + vy
-		return px, py
+	def movement(self, interval):
+		y = self.y
+		x = time = 0
+		time = interval
+		while y >= 0:
+			vx, vy = self.velocity(time)
+			x += vx
+			y += vy
+			time += interval
+			self.movements.append((x, y))
+		print(self.movements)
 
-	def update_position(self, t):
-		x, y = self.deslocamento(t)
-		if y >= 0:
-			print("%.2f | %.2f" %(x, y))
-			self.orbita.append((x * self.ESCALA , HEIGHT - y * self.ESCALA - self.diametro))
+	def update_position(self, frame):
+		if frame < len(self.movements) and frame >= 0:
+			self.position = frame
 
-def main(altura, velocidade, angulo):
-	run = True
-	clock = pygame.time.Clock()
-	t = 0
-	objeto = Objeto(0, altura, velocidade, angulo)
+class Game(Window):
+	def __init__(self, size, title, font):
+		super().__init__(size, title)
 
-	while run:
-		clock.tick(20)
-		WIN.fill(WHITE)
-		t += 1/20
+		self.velocity = 1/20
+		self.speed = 1
+		self.frame = 0
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
+	def run(self):
+		run = True
+		clock = pygame.time.Clock()
+		h = velocity = angle = None
 
-		objeto.update_position(t)
-		objeto.draw(WIN)
+		while h == None:
+			try:
+				h = float(input('Qual a altura o objeto a ser lançado? (metros) '))
+			except ValueError:
+				print("Valor incompatível")
+		while velocity == None or velocity <= 0:
+			try:
+				velocity = float(input('Qual a velocidade inicial (m/s): '))
+			except ValueError:
+				print("Valor incompatível")
+		while angle == None or (angle < 0 and angle >=90):
+			try:
+				angle = float(input('Qual o angulo que o objeto será lançado? (graus) '))
+			except ValueError:
+				print("Valor incompatível")
 
-		pygame.display.update()
+		self.init()
+		obj = Objeto(h, velocity, angle)
+		obj.movement(self.velocity)
+		self.append_component(obj)
 
-	pygame.quit()
+		while run:
+			clock.tick(20)
+			self.refresh_screen()
 
-main(altura, velocidade, angulo)
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					run = False
+				elif event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_SPACE:
+						self.handle_play()
+					if event.key == pygame.K_LEFT:
+						self.to_back()
+					if event.key == pygame.K_RIGHT:
+						self.forward()
+
+			if self.play:
+				self.frame += self.speed
+				obj.update_position(self.frame)
+
+		self.exit()
+
+if __name__ == "__main__":
+    game = Game((WIDTH, HEIGHT), "Movimento Retilíneo Uniformemente Variado", ("comicsans", 16))
+    game.run()
