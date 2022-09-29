8fa1fe4221c81d7265dd2ffccf4fa1e6fbab9c5b
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-09-29 19:55:13 -0300

    module base

---
+++ phisic_py/base/__init__.py
@@ -0,0 +1,9 @@
+from .window import Window
+from .inputs import InputBox
+from .menu import Menu
+
+__all__ = ['Window', 'InputBox', 'Menu']
+
+HEIGHT, WIDTH = 600, 1200
+WHITE = (210, 210, 210)
+BLACK = (0, 0, 0, 0.8)
