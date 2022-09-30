e17db5a14e3368259f7214085793eed216241eb7
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-09-30 19:50:08 -0300

    scale projetil.py

--- phisic_py/cinetica/projetil.py
+++ phisic_py/cinetica/projetil.py
@@ -6,10 +6,13 @@
 
 import pygame
 import math
-from base import Window
+import os
+from base import Window, Menu
 
 WIDTH, HEIGHT = 1200, 600
-WHITE, BLACK, GREY = (210, 210, 210), (0, 0, 0, .8), (128, 128, 128)
+WHITE, BLACK, GRAY = (210, 210, 210), (0, 0, 0, 0.8), (88, 88, 88)
+ABS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+IMG_PATH = os.path.join(ABS_PATH, 'sprites')
 
 class Objeto:
 	g = -9.807 		       	# Aceleração da gravidade (m/s^2)
@@ -18,43 +21,63 @@ class Objeto:
 	def __init__(self, h, velocity, angle):
 		self.x = 0											# Posição Vertical (m)
 		self.y = h													# Posição Horizontal (m)
+		self.angle = angle
+		self.v = velocity
 		self.vy = velocity * math.sin(math.radians(angle))		# Velocidade Inicial eixo y
 		self.vx = velocity * math.cos(math.radians(angle))		# Velocidade Inicial eixo x
 		self.diameter = 20 			# Raio (m)
+		self.menu = Menu(('clock.png', 'pointer.png', 'pointer.png', 'speedometer.png', 'speedometer.png', 'accelaration.png'))
+		self.font = pygame.font.SysFont('Arial', 12)
+
 		self.movements = []
+		self.velocities = []
 		self.position = 0
+		self.a = self.g
 		self.SCALE = 0
 
 	def draw(self, win):
 		if self.position > 2:
 			points = []
-			for movement in self.movements[0: self.position]:
+			for movement in self.movements[0: self.position + 1]:
 				points.append(self.transform(movement))
-			pygame.draw.lines(win, GREY, False, points, 2)
+			pygame.draw.lines(win, GRAY, False, points, 2)
 
 		pygame.draw.circle(win, BLACK, self.transform(self.movements[self.position]), self.diameter)
 
-	def transform(self, position):
-		self.SCALE = WIDTH / self.movements[-1][0]
-		x, y = position
-		return (x * self.SCALE - self.diameter, HEIGHT - y * self.SCALE - self.diameter)
+		timer = self.font.render('{:.2f} s'.format(self.position/20), True, BLACK)
+		posx = self.font.render('{:.1f} m (x)'.format(self.movements[self.position][0]), True, BLACK)
+		posy = self.font.render('{:.1f} m (y)'.format(self.movements[self.position][1]), True, BLACK)
+		velx = self.font.render('{:.2f} Km/h (x)'.format(self.velocities[self.position][0]), True, BLACK)
+		vely = self.font.render('{:.2f} Km/h (y)'.format(self.velocities[self.position][1]), True, BLACK)
+		a = self.font.render('{:.2f} m/s² (y)'.format(self.a), True, BLACK)
+		self.menu.draw(win, (timer, posx, posy, velx, vely, a))
 
-	def velocity(self, t):
-		Vx = self.vx * t
-		Vy = self.vy * t + self.g * math.pow(t, 2) / 2
-		return Vx, Vy
+	def transform(self, position):
+		if not self.SCALE:
+			if self.angle <= 45 :
+				self.SCALE = (WIDTH - self.diameter - 50) / self.movements[-1][0]
+			else:
+				hmax = (math.pow(self.v, 2) * math.pow(math.sin(math.radians(self.angle)), 2)) / (-2 * self.g)
+				self.SCALE = (HEIGHT - self.diameter - 50) / hmax
+		x, y = position
+		return (x * self.SCALE + 25, HEIGHT - y * self.SCALE - self.diameter)
+
+	def vel(self, t):
+		vx = self.vx * t
+		vy = self.vy * t + self.g * math.pow(t, 2) / 2
+		return vx, vy
 
 	def movement(self, interval):
-		y = self.y
-		x = time = 0
-		time = interval
+		x, y = self.x , self.y
+		time = 0
 		while y >= 0:
-			vx, vy = self.velocity(time)
-			x += vx
-			y += vy
+			vx, vy = self.vel(time)
+			x = self.x + vx
+			y = self.y + vy
 			time += interval
 			self.movements.append((x, y))
-		print(self.movements)
+			self.velocities.append((self.vx, self.vy + self.a * time))
+		return len(self.movements)
 
 	def update_position(self, frame):
 		if frame < len(self.movements) and frame >= 0:
@@ -67,6 +90,7 @@ class Game(Window):
 		self.velocity = 1/20
 		self.speed = 1
 		self.frame = 0
+		self.frames = 0
 
 	def run(self):
 		run = True
@@ -91,7 +115,7 @@ class Game(Window):
 
 		self.init()
 		obj = Objeto(h, velocity, angle)
-		obj.movement(self.velocity)
+		self.frames = obj.movement(self.velocity)
 		self.append_component(obj)
 
 		while run:
@@ -108,6 +132,8 @@ class Game(Window):
 						self.to_back()
 					if event.key == pygame.K_RIGHT:
 						self.forward()
+					if event.key == pygame.K_0 or event.key == pygame.K_KP0:
+						self.frame = 0
 
 			if self.play:
 				self.frame += self.speed
